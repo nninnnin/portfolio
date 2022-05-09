6538c3a6e5d2c61ff3e1aad169566ced32265815
nninnnin <nninnnin7@gmail.com>
2022-05-09 23:23:39 +0900

    Update styles; Adapt font as Spoqa han sans neo; Adapt react-markdown to parse markdown syntax

--- src/App.js
+++ src/App.js
@@ -1,12 +1,90 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useEffect, useState } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import ReactMarkdown from "react-markdown";
+
+import resumePath from "./data/resume.md";
 
 function App() {
-  return <Container>호잇 호잇..</Container>;
+  const [resume, setResume] = useState("");
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(resumePath);
+      console.log(res);
+      const result = await res.text();
+
+      setResume(result);
+    })();
+  }, []);
+
+  return (
+    <>
+      <GlobalStyle />
+      <Container>
+        <Header>@Justindglee</Header>
+        <ReactMarkdown>{resume}</ReactMarkdown>
+      </Container>
+    </>
+  );
 }
 
+const GlobalStyle = createGlobalStyle`
+  @import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css);
+
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    background-color: orangered;
+    color: white;
+    font-family: "Spoqa Han Sans Neo", "sans-serif";
+    font-weight: 300;
+    font-size: 0.8rem;
+  }
+
+  h1 {
+    margin: 0;
+    padding: 1em;
+    padding-bottom: 0;
+  }
+
+  ul, ol, li {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  li {
+    padding-left: 1em;
+  }
+
+  p {
+    padding-left: 1em;
+    line-height: 1.5em;
+
+    &::first-line {
+      line-height: 3em;
+      font-size: 1.5em;
+    }
+  }
+
+  img {
+    width: 300px;
+    margin: 1em;
+  }
+`;
+
 const Container = styled.div`
-  background-color: pink;
+  width: 30vw;
+  margin: 0 auto;
+  min-height: 100vh;
+
+  background-color: navy;
+`;
+
+const Header = styled.h1`
+  letter-spacing: 9px;
 `;
 
 export default App;
