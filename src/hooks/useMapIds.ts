330b5046fb5d1e8773696849893bc07389c3a83c
이동규(Justin) <34882520+nninnnin@users.noreply.github.com>
2023-09-11 18:38:34 +0900

    Fix useMapId

--- src/hooks/useMapIds.ts
+++ src/hooks/useMapIds.ts
@@ -5,16 +5,17 @@ import { v4 as uuid } from "uuid";
 type ElementWithId = Partial<Element> & { id: string };
 
 export default function useMapIds<Element>(list: Element[]): ElementWithId[] {
-  const result = useMemo<ElementWithId[]>(
-    map(
+  const result = useMemo<ElementWithId[]>(() => {
+    const mappedList = map(
       (elem: Element) => ({
         id: uuid(),
         ...elem,
       }),
       list
-    ),
-    [list]
-  );
+    );
+
+    return mappedList;
+  }, [list]);
 
   return result;
 }
